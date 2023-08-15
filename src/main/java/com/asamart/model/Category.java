94a0b64ca15e62b3e769bf0a79ed6c6c7e6f8c61
Younus <younusshaikh0313@gmail.com>
2023-08-15 19:34:11 +0530

    Changes by younus

---
+++ src/main/java/com/asamart/model/Category.java
@@ -0,0 +1,105 @@
+package com.asamart.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+//id, categoryname, description, createddate, createdBy image
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String categoryname;
+	private String description;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createddate;
+	
+	private String createdBy;
+	@Lob
+	private byte[] image;
+
+	@OneToMany(mappedBy = "categoryid")
+	private List<SubCategory> subCategory;
+
+    private List<SubCategory> subcategories;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCategoryname() {
+		return categoryname;
+	}
+
+	public void setCategoryname(String categoryname) {
+		this.categoryname = categoryname;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getCreateddate() {
+		return createddate;
+	}
+
+	public void setCreateddate(Date createddate) {
+		this.createddate = createddate;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public List<SubCategory> getSubCategory() {
+		return subCategory;
+	}
+
+	public void setSubCategory(List<SubCategory> subCategory) {
+		this.subCategory = subCategory;
+	}
+
+	
+	@PrePersist
+	protected void onCreate() {
+		createddate = new Date();
+	}
+}
