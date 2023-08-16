10a1dda0088d1f52f6b7ccdde3c7bdbf1eb04eb5
moresachin19 <moresachin158@gmail.com>
2023-08-16 15:12:19 +0530

    add globle exception - sachin more

---
+++ src/main/java/com/asamart/exceptions/EmptyInputException.java
@@ -0,0 +1,11 @@
+package com.asamart.exceptions;
+
+public class EmptyInputException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	// @Author Sachin more
+	public EmptyInputException() {
+
+	}
+
+}
