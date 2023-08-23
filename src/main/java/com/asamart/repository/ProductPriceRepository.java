8eef97e7e271409e7d17bc54170f3841126e05df
Younus <younusshaikh0313@gmail.com>
2023-08-23 18:27:27 +0530

    Changes by younus

--- src/main/java/com/asamart/repository/ProductPriceRepository.java
+++ src/main/java/com/asamart/repository/ProductPriceRepository.java
@@ -2,8 +2,8 @@ package com.asamart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.asamart.model.ProductPrice;
+
 @Repository
 public interface ProductPriceRepository extends JpaRepository<ProductPrice, Integer> {
 
--- src/main/java/com/asamart/service/impl/ProductPriceImpServicelementation.java
+++ src/main/java/com/asamart/service/impl/ProductPriceImpServicelementation.java
@@ -1,13 +1,20 @@
 package com.asamart.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.asamart.controller.ProductPriceController;
+import com.asamart.exceptions.CustomeExceptions;
 import com.asamart.model.ProductPrice;
 import com.asamart.repository.ProductPriceRepository;
 import com.asamart.service.ProductPriceService;
+
 @Service
 public class ProductPriceImpServicelementation implements ProductPriceService {
+	// @Younus Shaikh
+	private static final Logger logger = LoggerFactory.getLogger(ProductPriceController.class);
 
 	/* @Author: Sumit Gaikwad */
 	@Autowired
@@ -16,6 +23,29 @@ public class ProductPriceImpServicelementation implements ProductPriceService {
 	@Override
 	public ProductPrice savePriceDetails(ProductPrice priceDetails) {
 		return productPriceRepository.save(priceDetails);
+	}
+
+	// @Auther - Younus Shaikh
+	// Update ProductPrice by using Price Id
+
+	@Override
+	public ProductPrice updateProductPriceById(int id, ProductPrice productPrice) {
+		logger.info("Update the productPrice details by Id");
+		int ppid = productPrice.getPriceId();
+		ppid = id;
+		ProductPrice productPrice2 = new ProductPrice();
+		try {
+			productPrice2 = productPriceRepository.findById(id).get();
+			productPrice2.setRegularPrice(productPrice.getRegularPrice());
+			productPrice2.setSellPrice(productPrice.getSellPrice());
+			productPrice2.setUnit(productPrice.getUnit());
+			productPrice2.setDeleted(false);
+			return productPriceRepository.save(productPrice2);
+
+		} catch (Exception e) {
+			throw new CustomeExceptions("ProductPrice id " + id + " is incorrect..");
+		}
+
 	}
 
 }
